54a39d7238cb92b0e0bae506baf9a5e59e32805b
vanshg11 <vanshvijay038@gmail.com>
2024-06-29 18:58:17 +0530

    Update Server

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,9 +2,9 @@ import app from "./app.js";
 import cloudinary from "cloudinary";
 const PORT = process.env.PORT || 4000;
 cloudinary.config({
-  cloud_name: "dwb76ojmv",
-  api_key: "383994124972961",
-  api_secret: "2zB4oiPsj_WDyeYh0v7Lr0MwEII",
+  cloud_name: "sec2",
+  api_key: "sec1",
+  api_secret: "sec",
 });
 // app.post("/api/v1/user/register", (req, res) => {
 //   const { username, password } = req.body;
